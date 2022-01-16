1a1718d6397b66c0f142ae11684deeca6afef9aa
chankim <videoal@gmail.com>
2022-01-16 23:43:42 +0900

    add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//import Potato from "./Potato";
+
+//컴포넌트를 내부에 사용할 수도 있다.
+function Potato() {
+  return <h3>I hate potato</h3>;
+}
 
 function App() {
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: 16,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react"; // 리액트가 JSX를 이해하기 위해..
+
+//컴포넌트는 대문자로 시작!!
+function Potato() {
+  return <h3>I love potato</h3>;
+}
+
+export default Potato;
